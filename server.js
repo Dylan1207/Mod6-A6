bacd606fdb6718271c403d40ccdd4af8499311be
Dylan Drum <dmdrum@pointpark.edu>
2026-04-07 13:19:43 -0400

    enable cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const pool = require('./db');
 require('dotenv').config();
 
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
